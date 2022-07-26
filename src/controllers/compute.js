1ce1a5845bd4b2a200d4ba400443c5629f014095
afudoh kachi <fudosman@gmail.com>
2022-07-26 08:30:44 +0100

    initial update

--- src/controllers/compute.js
+++ src/controllers/compute.js
@@ -1,10 +1,48 @@
- const compute = (req,res) => {
-  console.log("successfully hit the compute server");
-  return res.status(200).json({
-    status: "success",
-    message: "successful request to the compute"
-  });
+ const compute = (req, res) => {
+   try {
+     const {
+       ID,
+       Amount,
+       Currency,
+       CustomerEmail,
+       SplitInfo
+     } = req.body;
+     if (SplitInfo.length > 0 && SplitInfo.length < 20){
+      SplitInfo.map((splitEntites)=>splitEntites)
+     }
+    
+    //  splitTypes
+    if(SplitType === "FLAT"){
+      
+    }
+    if(SplitType === "PERCENTAGE"){
+
+    }
+    if(SplitType === "RATIO"){
+
+    }
+
+     console.log("now on it again");
+     return res.status(200).json({
+       ID: null,
+       Balance: null,
+       SplitBreakdown: [{
+           splitEntityId: null,
+           Amount: null
+         },
+         {
+           splitEntityId: null,
+           Amount: null
+         }
+       ]
+     });
+   } catch (error) {
+     console.error(error)
+     res.status(500).send({
+       status: "error",
+       message: "try again later, server error"
+     });
+   }
  };
 
  module.exports = compute;
- 
