57f459e750c66e03226851336274e24abb4d4450
afudoh kachi <fudosman@gmail.com>
2022-07-26 12:50:54 +0100

    changes

--- src/controllers/compute.js
+++ src/controllers/compute.js
@@ -1,22 +1,24 @@
 const ComputeOps = require('../utility/splitRules');
 
 const compute = async (req, res) => {
-  //  try {
+   try {
     const payload = req.body;
     const {Amount, SplitInfo} = payload;
     const computeOps = new ComputeOps(Amount, SplitInfo);
+
+    computeOps.checkParticipants();
     computeOps.FlatOps();
     computeOps.PercentageOps();
     computeOps.RatioOps();
     
     
-  //  } catch (error) {
-  //    console.error(error);
-  //    res.status(500).send({
-  //      status: "error",
-  //      message: "try again later, server error"
-  //    });
-  //  }
+   } catch (error) {
+     console.error(error);
+     res.status(500).send({
+       status: "error",
+       message: "try again later, server error"
+     });
+   }
  };
 
  module.exports = compute;
